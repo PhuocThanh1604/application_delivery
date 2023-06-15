37fb65adddd448f79511eac3a470614ae3052501
thanhvp <phuocthanh20cgh@gmail.com>
2023-06-15 15:25:03 +0700

    create login

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title:'Delivery Udemy',
+      debugShowCheckedModeBanner: false,
       initialRoute: '/',
       getPages: [
         GetPage(name: '/', page: ()=>LoginPage())
