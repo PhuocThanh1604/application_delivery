f24c54cafa941b9dd411a1a4dade2cd1d6b5ad7c
thanhvp <phuocthanh20cgh@gmail.com>
2023-06-15 15:22:16 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/thanhvp/udemy_flutter_delivery/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.thanhvp.udemy_flutter_delivery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:udemy_flutter_delivery/src/pages/login/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initSate(){
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title:'Delivery Udemy',
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: ()=>LoginPage())
+      ],
+      navigatorKey: Get.key,
+    );
+
+  }
+}
