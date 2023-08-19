dfc1859a02f57b5e16616dd0cd327112c00e2e85
thanhvp <phuocthanh20cgh@gmail.com>
2023-08-19 14:02:00 +0700

    config gg map

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,8 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    <!-- config map!-->
+  GMSServices provideAPIKey:@"AIzaSyD_Uu1btO6mWLrRrPBuw84FkQuLK6tFuLU";
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
