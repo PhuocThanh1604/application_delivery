49e2e4e1d3b9a41bb3114c977f85d17a4d59d732
thanhvp <phuocthanh20cgh@gmail.com>
2023-08-19 14:00:48 +0700

    config gg map

--- lib/src/environment/environment.dart
+++ lib/src/environment/environment.dart
@@ -1,4 +1,7 @@
 class Enviroment {
   static const String API_URL ="http://192.168.0.112:3000/";
   static const String API_URL_OLD ="192.168.0.112:3000";
+
+
+  static const String API_KEY_MAPS ="AIzaSyD_Uu1btO6mWLrRrPBuw84FkQuLK6tFuLU";
 }
